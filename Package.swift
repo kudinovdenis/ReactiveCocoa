072c50499a86179a67f172fa839a1a7a7dd5ac38
Denis Kudinov <Denis.Kudinov@acronis.com>
2017-09-20 15:03:16 +0300

    package to Package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "ReactiveCocoa",
 	dependencies: [
-		.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", Version(2, 1, 0, prereleaseIdentifiers: ["alpha.2"]))
+		.Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", Version(2, 1, 0, prereleaseIdentifiers: ["alpha.2"]))
 	],
 	swiftLanguageVersions: [3, 4]
 )
